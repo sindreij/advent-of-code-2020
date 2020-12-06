ad397cd9a5ea7e8fbbcca7ce1912738cf24e7c5e
Sindre Ilebekk Johansen <sindre@sindrejohansen.no>
2020-12-06 11:25:37 +0100

    Aoc05

--- aoc05/src/main.rs
+++ aoc05/src/main.rs
@@ -23,8 +23,7 @@ fn part1(input: &str) -> Result<u32> {
     Ok(input
         .split('\n')
         .filter(|line| !line.is_empty())
-        .filter_map(|line| parse_seat_str(line).ok())
-        .map(|(row, col)| row * 8 + col)
+        .filter_map(|line| parse_seat_str2(line).ok())
         .max()
         .unwrap())
 }
@@ -33,8 +32,7 @@ fn part2(input: &str) -> Result<u32> {
     let used_seats = input
         .split('\n')
         .filter(|line| !line.is_empty())
-        .filter_map(|line| parse_seat_str(line).ok())
-        .map(|(row, col)| row * 8 + col)
+        .filter_map(|line| parse_seat_str2(line).ok())
         .collect::<HashSet<_>>();
 
     let min_seat = *used_seats.iter().min().unwrap();
@@ -49,25 +47,36 @@ fn part2(input: &str) -> Result<u32> {
     Ok(free_seats[0])
 }
 
-fn parse_seat_str(input: &str) -> Result<(u32, u32)> {
-    let mut row_min = 0;
-    let mut row_max = 128;
+// Before figuring out it was possible to just convert to binary:
+//
+// fn parse_seat_str(input: &str) -> Result<u32> {
+//     let mut row_min = 0;
+//     let mut row_max = 128;
 
-    let mut col_min = 0;
-    let mut col_max = 8;
+//     let mut col_min = 0;
+//     let mut col_max = 8;
 
-    for chr in input.chars() {
-        match chr {
-            'F' => row_max = row_max - (row_max - row_min) / 2,
-            'B' => row_min = row_min + (row_max - row_min) / 2,
+//     for chr in input.chars() {
+//         match chr {
+//             'F' => row_max = row_max - (row_max - row_min) / 2,
+//             'B' => row_min = row_min + (row_max - row_min) / 2,
 
-            'L' => col_max = col_max - (col_max - col_min) / 2,
-            'R' => col_min = col_min + (col_max - col_min) / 2,
-            _ => bail!("Invalid chr, {}", chr),
-        }
-    }
+//             'L' => col_max = col_max - (col_max - col_min) / 2,
+//             'R' => col_min = col_min + (col_max - col_min) / 2,
+//             _ => bail!("Invalid chr, {}", chr),
+//         }
+//     }
 
-    Ok((row_min, col_min))
+//     Ok(row_min * 8 + col_min)
+// }
+
+fn parse_seat_str2(input: &str) -> Result<u32> {
+    Ok(u32::from_str_radix(
+        &input
+            .replace(&['B', 'R'][..], "1")
+            .replace(&['F', 'L'][..], "0"),
+        2,
+    )?)
 }
 
 #[cfg(test)]
@@ -76,6 +85,7 @@ mod tests {
 
     #[test]
     fn test_parse_seat_str() {
-        assert_eq!(parse_seat_str("FBFBBFFRLR").unwrap(), (44, 5));
+        // assert_eq!(parse_seat_str("FBFBBFFRLR").unwrap(), 44 * 8 + 5);
+        assert_eq!(parse_seat_str2("FBFBBFFRLR").unwrap(), 44 * 8 + 5);
     }
 }
